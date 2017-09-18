124f25956ac22cc7a50e5afe17f423c70c2b09b7
Antonio Valdez <joanvaro90@gmail.com>
2017-09-18 13:56:50 -0500

    Adding function to get the specs it is not working but it is running without errors

--- DoorsScript.rb
+++ DoorsScript.rb
@@ -1,20 +1,35 @@
 #!/usr/bin/ruby
 
-fileName = ARGV[0]
-className = String.new()
-methods = Hash.new()
-attributes = Hash.new()
+$fileName = ARGV[0]
+$className = String.new()
+$methods = Hash.new()
+$attributes = Hash.new()
 
-data = File.readlines(fileName)
+$data = File.readlines($fileName)
 
-data.each do |lines|
-  if lines.match?(/@brief/) 
-    if lines.match?(/class/)
+def getSpecs(methodName) 
+  iterator = $data.find_index(methodName)
+  puts iterator
+end
+
+
+$data.each do |lines|
+  # It is looking for comments only
+  if lines.match(/@brief/) 
+    # It is looking for the class name
+    if lines.match(/class/)
       arry = lines.split
-      className = arry[-2]
+      $className = arry[-2]
+
+    else
+
+      func = lines.split
+      $methods[func[-1]] = Hash.new()
+      getSpecs(lines)
     end
   end
 
 end
 
-puts className
+puts $className
+puts $methods
